282a0d8e112cd4c6badc0dfbe87dbaa328d62fe6
Himanshu Verma <66989032+Hi-manshu21@users.noreply.github.com>
2026-05-11 20:44:29 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (32.12%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -3,13 +3,16 @@ class Solution {
         int xor1 = 0;
         int xor2 = 0;
 
-        for(int i=0;i<nums.length+1;i++){
-            xor1 = xor1^i;
-        }
+        // for(int i=0;i<nums.length+1;i++){
+        //     xor1 = xor1^i;
+        // }
 
         for(int i=0;i<nums.length;i++){
             xor2 = xor2^nums[i];
+            xor1 = xor1^i;
         }
+        
+        xor1 = xor1^nums.length;
 
         return xor1^xor2;
     }
