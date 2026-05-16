ed780dfe6852e567a7787f6926bbfa7baa0231aa
Himanshu Verma <66989032+Hi-manshu21@users.noreply.github.com>
2026-05-16 13:25:33 +0530

    Time: 6 ms (44.87%) | Memory: 44.2 MB (82.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,29 +1,51 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+
+//         HashMap<Character, Integer> map = new HashMap<>();
+
+//         for(int i=0;i<s.length();i++){
+//             char ch = s.charAt(i);
+//             map.put(ch, map.getOrDefault(ch,0)+1);
+//         }
+
+//         for(int i=0;i<t.length();i++){
+//             char ch = t.charAt(i);
+//             if(map.get(ch) != null){
+//                 if(map.get(ch) == 1){
+//                     map.remove(ch);
+//                 }else{
+//                     map.put(ch,map.get(ch)-1);
+//                 }
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         return map.isEmpty();
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()){
             return false;
         }
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        int [] arr = new int[26];
 
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch,0)+1);
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
 
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-            if(map.get(ch) != null){
-                if(map.get(ch) == 1){
-                    map.remove(ch);
-                }else{
-                    map.put(ch,map.get(ch)-1);
-                }
-            }
-            else{
+        for(int i=0;i<26;i++){
+            if(arr[i] != 0){
                 return false;
             }
         }
-        return map.isEmpty();
+        return true;
     }
 }
